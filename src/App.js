4aab66e41b7f374510de83e279d6f7d6d8320c7b
Joanne <joannesayswu@gmail.com>
2019-08-07 11:12:44 -0700

    Basic bluepoint done, App component state and methods done. Need to be tested later

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import BlogPost from './BlogPost';
+import BlogForm from './BlogForm';
 import './App.css';
+import { stat } from 'fs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** App
+ * 
+ * state:
+ * - blogs: {
+      id : {
+        title: string
+        content: string
+      }
+    }
+ */
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { blogs: {} };
+    this.addPost = this.addPost.bind(this);
+    this.editPost = this.editPost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+  }
+
+
+  /** given newPostObj = {id, title, description}, 
+   * add to state in form {id : { title, description }} 
+   * 
+   * TODO: CHECK IF THIS WORKS
+   */
+
+  addPost(newPostObj) {
+    const { id, ...content } = newPostObj;
+    this.setState(state =>({
+      blogs: {...state.blogs, [id]: content }
+    }))
+  }
+
+  /** given editedPostObj = {id, title, description}, 
+   * edit post in state in form {id : { title, description }} 
+   */
+  editPost(editedPostObj) {
+    const { id, ...content } = editedPostObj;
+    this.setState(state =>({
+      blogs: {...state.blogs, [id]: content }
+    }))
+
+  }
+
+  /** given post id, delete post from state */
+  deletePost(id) {
+    if(this.state.blogs[id]===undefined){
+      return
+    }
+    let copiedBlogs = {...this.state.blogs};
+    delete copiedBlogs[id];
+
+    this.setState(state =>({
+      blogs: copiedBlogs
+    }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
